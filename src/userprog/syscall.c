8e1a594dc837db0b6fda5a03e4638b597f76ad0e
Soowon Jeong <jeong0982@kaist.ac.kr>
2020-09-07 12:58:46 +0000

    Initialize system call

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,20 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include <devices/shutdown.h>
+#include <threads/thread.h>
+#include <filesys/filesys.h>
 
 static void syscall_handler (struct intr_frame *);
+
+void halt (void);
+void exit (int);
+
+void check_address (void *addr) {
+  if (0x8048000 > addr || 0xc0000000 < addr) {
+    thread_exit ();
+  }
+}
 
 void
 syscall_init (void) 
@@ -15,6 +27,42 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  printf("syscall num : %d\n", *(uint32_t *)(f->esp));
   printf ("system call!\n");
+
+  int *arg;
+  void *esp = malloc(4);
+
+  switch (*(uint32_t *)(f->esp)) {
+    case SYS_HALT:
+      {
+        halt ();
+        NOT_REACHED ();
+        break;
+      }
+    case SYS_EXIT:
+      {
+
+      }
+    default:
+      thread_exit ();
+  }
   thread_exit ();
+}
+
+void halt(void) {
+  shutdown_power_off ();
+}
+
+void exit (int status) {
+  printf("%s: exit(%d)\n", thread_current()->name, status);
+  thread_exit ();
+}
+
+bool create (const char *file, unsigned initial_size) {
+  return filesys_create (file, initial_size);
+}
+
+bool remove (const char *file) {
+  return filesys_remove (file);
 }
